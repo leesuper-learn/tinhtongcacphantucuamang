02f8be5afb92fe0ec08544a173ed0af1a4f021f4
Sillver Bullet <161559713+leesuper-learn@users.noreply.github.com>
2024-03-05 11:22:36 +0800

    Initial commit

---
+++ tinhtongcacphantucuamang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tinhtongcacphantucuamang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            num[0] = 2;
+            Console.WriteLine(num[0]);
+            num[1] = 5;
+            num[2] = 9;
+            Console.WriteLine(num[2]);
+            num[3] = 6;
+            num[4] = 7;
+            Console.WriteLine(num[4]);
+            int tong = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                tong = tong + num[i];
+            }
+            Console.WriteLine("Tong la: " + tong.ToString());
+            Console.ReadKey();
+        }
+    }
+}
